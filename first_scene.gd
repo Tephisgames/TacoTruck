e231ebbb55612d33226492cfa38d622112eeb3b1
xXLyrickXx <lyrickfisher@gmail.com>
2025-01-06 04:06:26 -0800

    code clean
    condensed a few things and renamed some nodes

--- first_scene.gd
+++ first_scene.gd
@@ -7,13 +7,22 @@ var tacos: int=0
 
 
 func _ready()-> void: 
-	(get_node("Label")as Label).text = "Tacos : %s"%tacos
+	update_label_tacos()
 	##This Line Specifies label as Amount of Tacos
 	
-	(get_node("Button")as Button).pressed.connect(_on_button_pressed)
+	($Taco_Button as Button).pressed.connect(_on_button_pressed_tacos)
 	##This line connects the button to the pressed function 
 
-func _on_button_pressed()-> void:
+##func is too call back taco amount and updating this updates all
+func update_label_tacos()->void:
+	($Taco_Amount as Label).text = "Tacos : %s"%tacos
+	##renamed nodes and func to match object funtion
+	##$ is same as to getnode
+
+func create_tacos()->void:
 	tacos += 1
-	(get_node("Label")as Label).text = "Tacos : %s"%tacos
+	update_label_tacos()
+
+func _on_button_pressed_tacos()-> void:
+	create_tacos()
 	##This Line Specifies label as Amount of Tacos after button press
